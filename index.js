6539d4874359c9ed64566d80d24a44c8ba8ca18e
Victor-Mannelli <imp.mannelli@gmail.com>
2022-11-12 18:29:09 -0300

    feat: working participants post and get routs for the api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import joi from "joi";
+import dayjs from "dayjs";
 import { MongoClient } from "mongodb";
 
 const app = express();
@@ -9,16 +11,96 @@ app.use(cors());
 app.use(express.json());
 dotenv.config();
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
+const participantsScheme = joi.object({
+	name: joi.string().required(),
+});
+const messagesScreme = joi.object({
+	to: joi.string().required(),
+	text: joi.string().required(),
+	type: joi.any().valid("message", "private_message").required(),
+});
 
-try {
-	await mongoClient.connect()
-} catch (error) {
-	console.log(error)
+let mongoClient = undefined;
+async function connectMongo() {
+	try {
+		if (mongoClient === undefined) {
+			const conection = new MongoClient(process.env.MONGO_URI);
+			await conection.connect();
+			mongoClient = conection;
+			return conection;
+		} else {
+			return mongoClient;
+		}
+	} catch (error) {
+		console.log(error);
+	}
 }
 
-let db = mongoClient.db("")
+app.post("/participants", async (req, res) => {
+	const mongoClient = await connectMongo();
+	const body = req.body;
+	const userName = req.body.name;
 
+	try {
+		const { error } = participantsScheme.validate(body);
+		if (error) return res.sendStatus(422).send(error.details.map((e) => e.message));
 
+		const isLoggedIn = await mongoClient
+			.db("batepapoUol")
+			.collection("participants")
+			.findOne({ name: userName });
+		if (isLoggedIn) return res.sendStatus(409);
 
-app.listen(5000, () => console.log("Server running in port 5000"));
+		await mongoClient.db("batepapoUol").collection("participants").insertOne({
+			name: userName,
+			lastStatus: Date.now(),
+		});
+		res.sendStatus(201);
+	} catch (error) {
+		res.sendStatus(422);
+	}
+});
+
+app.get("/participants", async (req, res) => {
+	const mongoClient = await connectMongo();
+	try {
+		const partList = await mongoClient
+			.db("batepapoUol")
+			.collection("participants")
+			.find({})
+			.toArray();
+
+		res.status(201).send(partList);
+	} catch {
+		res.sendStatus(422);
+	}
+});
+
+app.post("/messages", async (req, res) => {
+	const mongoClient = await connectMongo();
+	const body = req.body;
+	const { user } = req.headers;
+
+	try {
+		const { error } = messagesScreme.validate(body);
+		if (error) return res.status(422).send(error.details.map((e) => e.message));
+
+		const userExists = await mongoClient
+			.db("batepapoUol")
+			.collection("participants")
+			.findOne({ name: user });
+		if (userExists) return res.sendStatus(422);
+
+		await mongoClient.db("batepapoUol").collection("messages").insertOne({
+			from: user, 
+			to: body.to, 
+			text: body.text, 
+			type: body.type, 
+			time: dayjs().format('HH:MM:SS')
+		});
+
+	} catch(error) {
+		console.log(error)
+	}
+});
+app.listen(5000);
